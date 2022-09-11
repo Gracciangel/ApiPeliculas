6f2e1972b51d472286ecc2a7f4d9f380283fe9a9
Gracciangel <gracciangel@outlook.com>
2022-09-11 13:24:35 -0300

    se modificaron archivos js

--- scripts/vistaUsuario.js
+++ scripts/vistaUsuario.js
@@ -1,35 +1,15 @@
-// array´s de peliculas
 
-const movieAction = [
-  (argo = {
-    nombre: "argo",
-    duracion: "108 min ",
-  }),
-  (pianista = {
-    nombre: "El Pianista",
-    duracion: "108 min ",
-  }),
-  (sotano = {
-    nombre: "El Sotano",
-    duracion: "108 min ",
-  }),
-];
-const movieAdventure = [];
-const movieNationals = [];
-const movieComedy = [];
 
 // control del DOM
 const peliculasApi = document.getElementById("peliculas");
 const action = document.getElementById("accion");
 const flex = document.getElementById("flexPrint");
+// busqueda
+let seacrh = document.getElementById('buscar')
+let btnSearch = document.getElementById('bt-buscar')
 
 // pagina
 let pagina = 1;
-
-action.onclick = () => {
-  console.log("peliculas.json");
-};
-
 const peli = () => {
   fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=aaf8812b1d1a70feeec14d75ddd78b5f&language=es-MX&page=${pagina}`
@@ -37,18 +17,43 @@ const peli = () => {
     .then((res) => res.json())
     .then((res) => {
       const resultado = res.results ;
+  
      for(e of resultado){
       peliculasApi.innerHTML+=`
       <img  src="https://image.tmdb.org/t/p/w500/${e.poster_path}" alt="" class="imgPath" id="img${e.id}">
       `;
-    }
-    resultado.forEach(element => {
-     let img = element.id; 
-     document.getElementById(`img${img}`).addEventListener('click',()=>{
-      alert(element.title)
-     })
-    });
-    });
-};
-peli();
+      btnSearch.onclick=(e)=>{
+          e.preventDefault()
+          let buscarPelicula = seacrh.value; 
+          for(e of resultado){
+            e.title.innerText= e.title.toLowerCase() ;
+            if(buscarPelicula == e.title){
+             alert(e.title)
+            }
+              
+          }
+        
+      
+        }
+   
 
+      
+    }
+   
+
+    resultado.forEach(element => {
+     let img = `img${element.id}` 
+     document.getElementById(img).addEventListener('click',()=>{
+    Swal.fire({
+      icon: "info",
+      title: `${element.title}`,
+      text: `${element.overview}`,
+    })
+  })
+});
+});
+
+};
+
+// llamdo de funciones
+peli();

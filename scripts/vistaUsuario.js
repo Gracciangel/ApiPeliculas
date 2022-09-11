5ba1d5beb748b59be194e3d26acd62fb5bc91d3d
Gracciangel <gracciangel@outlook.com>
2022-09-11 15:12:40 -0300

    se aplicaron ajustes de paginacion

--- scripts/vistaUsuario.js
+++ scripts/vistaUsuario.js
@@ -1,15 +1,46 @@
 
 
 // control del DOM
+
 const peliculasApi = document.getElementById("peliculas");
 const action = document.getElementById("accion");
 const flex = document.getElementById("flexPrint");
+const btnSig = document.getElementById('btnSig')
+const numeroDePagina = document.getElementById('numeroDePagina')
+const btnAnt = document.getElementById('btnAnt')
 // busqueda
 let seacrh = document.getElementById('buscar')
 let btnSearch = document.getElementById('bt-buscar')
 
 // pagina
 let pagina = 1;
+
+// funciones
+
+
+const pagination=()=>{
+  btnSig.onclick=()=>{
+   if(pagina <1000){
+     pagina+=1
+     peliculasApi.innerHTML=
+  numeroDePagina.innerText=pagina ;
+  peli()
+   }
+   }
+  }
+  btnAnt.onclick=()=>{
+    if(pagina>1){
+      pagina-=1
+    peliculasApi.innerHTML=
+    numeroDePagina.innerText=pagina
+    peli()
+    }
+  }
+
+
+
+
+
 const peli = () => {
   fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=aaf8812b1d1a70feeec14d75ddd78b5f&language=es-MX&page=${pagina}`
@@ -28,7 +59,7 @@ const peli = () => {
           for(e of resultado){
             e.title.innerText= e.title.toLowerCase() ;
             if(buscarPelicula == e.title){
-             alert(e.title)
+             alert(e.overview)
             }
               
           }
@@ -57,3 +88,4 @@ const peli = () => {
 
 // llamdo de funciones
 peli();
+pagination()
